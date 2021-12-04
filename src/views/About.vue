46d848c3a46f07672d807d53883594d5a01678d0
Hanwen Geng <genghanwen06@gmail.com>
2021-12-03 18:52:17 -0800

    add

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about"><!--github team page-->
+    <h1 class="title">About Us</h1>
+    <h2 class="title">Our Website</h2>
+    <p class="intro">With our car rental company CarToGo, we want to offer people an alternative to achieving automobile freedom without the hassle of traditional car ownership. So you can travel more, and worry less.</p>
+    <section class="member"></section>
+    <h2 class="title">Our Team</h2>
+      <div class="row">
+        <div class="column">
+          <div class="member">
+            <img src="/member1.jpg" alt="Khan" style="width:100%">
+            <div class="container">
+              <h2>Khan</h2>
+              <p class="title">Team lead</p>
+              <p>Description of me.</p>
+            </div>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
