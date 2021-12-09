23ddf4f3ccfba8adadd39107a47056aa15ab9b75
Khanh Nguyen <knguyenn11@gmail.com>
2021-12-08 19:12:53 -0800

    Fix merge conflict

--- src/views/PaymentForm.vue
+++ src/views/PaymentForm.vue
@@ -193,10 +193,6 @@ export default {
       title: "Update Payment Form",
 
       //Data field variables
-<<<<<<< HEAD
-=======
-      //userEmail: {
->>>>>>> Pushing what i have for the signup
       name: "",
 
       userStreet: "",
@@ -210,10 +206,6 @@ export default {
       cardYear: "",
       cardCode: "",
       defaultPay: "",
-<<<<<<< HEAD
-=======
-      //},
->>>>>>> Pushing what i have for the signup
       states: [
         "Alabama",
         "Alaska",
