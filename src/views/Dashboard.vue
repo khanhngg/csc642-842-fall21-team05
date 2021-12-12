a6e861944ed8904dd1e768b403e92150e34faaff
Hanwen Geng <genghanwen06@gmail.com>
2021-12-12 05:30:47 -0800

    add filter

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -20,14 +20,9 @@
                 <!-- Filters -->
                 <section class="row form-section mb-4">
                     <div class="bg-light border rounded-3 p-3">
-                        <form class="d-flex" @submit.prevent="handleSubmit">
-                        <input
-                        class="form-control me-2"
-                        type="search"
-                        placeholder="Search By Vehicle ID"
-                        aria-label="Search"
-                        />
-                        <button class="btn btn-primary-theme" type="submit">Search</button>
+                        <form class="d-flex" >
+                            <input class="form-control me-2" type="text" v-model="search" placeholder="Search By Vehicle ID"/>
+                            <!-- <button class="btn btn-primary-theme" @click="SearchID">Search</button> -->
                         </form>
                         <h5 class="fw-bold">Filters</h5>
                         <div class="col-3 mb-3">
@@ -37,7 +32,7 @@
                             </select>
                         </div>
                         <div class="col-3 mb-3">
-                            <select class="form-select">
+                            <select class="form-select" v-model="searchmake">
                                 <option value="">Filter By Make</option>
                                 <option value="Toyota">Toyota</option>
                                 <option value="Honda">Honda</option>
@@ -46,12 +41,12 @@
                             </select>
                         </div>
                         <div class="col-3 mb-3">
-                            <select class="form-select">
+                            <select class="form-select" v-model="searchprice">
                                 <option value="">Filter By Price</option>
-                                <option value="Toyota">0-100</option>
-                                <option value="Honda">100-200</option>
-                                <option value="Porsche">200-300</option>
-                                <option value="Tesla">300-400</option>
+                                <option value="0-100">0-100</option>
+                                <option value="100-200">100-200</option>
+                                <option value="200-300">200-300</option>
+                                <option value=">300">>300</option>
                             </select>
                         </div>
                     </div>
@@ -61,7 +56,8 @@
                 <!-- List of cars -->
                 <h6 class="fw-bold text-uppercase">Showing 1-10 out of 50 cars</h6>
                 <section class="col-9 pe-0">
-                    <div v-for="(car, index) in cars" :key="car.id" class="card mb-3">
+                    <!-- <div v-for="(car, index) in cars" :key="car.id" class="card mb-3"> -->
+                    <div v-for="(car, index) in filteredList" :key="car.id" class="card mb-3">
                         <div class="row g-0">
                             <div class="col-md-4">
                             <img :src="`${car.image}`" class="img-fluid rounded-start" alt="Car Image">
@@ -99,30 +95,40 @@
     export default {
         data() {
             return {
-            car: {
-                id: "",
-                make: "",
-                model: "",
-                year: "",
-                type: "",
-                fuel: "",
-                mpg: "",
-                door: "",
-                seat: "",
-                suitcase: "",
-                addon: [],
-                image: "",
-                price: "",
-                description: "",
-            },
-            cars: [],
+                car: {
+                    id: "",
+                    make: "",
+                    model: "",
+                    year: "",
+                    type: "",
+                    fuel: "",
+                    mpg: "",
+                    door: "",
+                    seat: "",
+                    suitcase: "",
+                    addon: [],
+                    image: "",
+                    price: "",
+                    description: "",
+                },
+                cars: [],
+                search: '',
+                searchmake: '',
+                searchprice: '',
             };
         },
         computed: {
             years: function () {
                 const year = new Date().getFullYear()
                 return Array.from({length: year - 2009}, (value, index) => 2010 + index)
-            }, 
+            },
+            filteredList() {
+                return this.cars.filter(car => {
+                    return car.id.toString().includes(this.search)
+                    &&car.make.toLowerCase().includes(this.searchmake.toLowerCase())
+                    //&&car.price.toString().includes(this.findPrice(car.price,this.searchprice))
+                })
+            },
         },
         methods: {
             onAdd: function () {
@@ -131,6 +137,40 @@
             async onDelete(index) {
                 this.cars.splice(index, 1);
                 localStorage.setItem("cars", JSON.stringify(this.cars));
+            },
+            findPrice: function (price,searchprice) {
+                if (searchprice=="0-100") {
+                    if (price>0 && price<=100){
+                        return price
+                    }
+                    else{
+                        return -1
+                    }
+                }
+                if (searchprice=="100-200"){
+                    if (price>100 && price<=200){
+                        return price
+                    }
+                    else{
+                        return -1
+                    }
+                }
+                if (searchprice=="200-300"){
+                    if (price>200 && price<=300){
+                        return price
+                    }
+                    else{
+                        return -1
+                    }
+                }
+                if (searchprice==">300"){
+                    if (price>300){
+                        return price
+                    }
+                    else{
+                        return -1
+                    }
+                }
             },
         },
         async created() {
