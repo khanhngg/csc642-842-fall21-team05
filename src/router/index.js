56cc98922935c779f8f952c7a72010cf0f6ce56d
cny257 <56347703+cny257@users.noreply.github.com>
2021-12-13 01:10:00 -0800

    update from run lint

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,7 @@ import EditSummary from "../views/EditSummary.vue";
 import ReservationSummaryGuest from "../views/ReservationSummaryGuest.vue";
 import ReservationSummaryLoggedin from "../views/ReservationSummaryLoggedin.vue";
 import ReservationSummaryLoggedinCanceled from "../views/ReservationSummaryLoggedinCanceled.vue";
+import ProfileEdit from "../views/ProfileEdit.vue";
 
 const routes = [
   {
@@ -179,6 +180,11 @@ const routes = [
     path: "/reservation/summary/loggedin/canceled",
     name: "ReservationSummaryLoggedinCanceled",
     component: ReservationSummaryLoggedinCanceled,
+  },
+  {
+    path: "/profile/edit",
+    name: "ProfileEdit",
+    component: ProfileEdit,
   },
 ];
 
